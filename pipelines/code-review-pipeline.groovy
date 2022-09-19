003f2e76a091d95fc8b178d3d1928289456ba1e7
Yevhen Bereza <Yevhen_Bereza@epam.com>
2022-09-19 18:55:35 +0300

    add code -review-pipeline

---
+++ pipelines/code-review-pipeline.groovy
@@ -0,0 +1,24 @@
+node {
+    parameters {
+        string(name: 'GIT_URL', defaultValue: '', description: 'Git repo url')
+        string(name: 'GIT_BRANCH', defaultValue: 'task-3', description: 'Review')
+        string(name: 'GIT_CREDENTIALS', defaultValue: 'git-creds', description: 'Git repo credentials')
+        string(name: 'ENV', defaultValue: 'dev', description: 'Build environment')
+    }
+
+    stages {
+        stage("test") {
+            echo "test"
+        }
+        stage("checkout") {
+            checkout scm: [
+                    $class           : 'GitSCM',
+                    userRemoteConfigs: [[url: params.GIT_URL , credentialsId: params.GIT_CREDENTIALS]],
+                    branches         : [[name: "refs/heads/${params.GIT_BRANCH}"]],
+            ], poll: false
+        }
+        stage("hadolint-check") {
+            hadolint Dockerfile
+        }
+    }
+}
